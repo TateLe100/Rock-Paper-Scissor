8796adbec89ff896fa24e7ac63d8ae892113047e
TateLe100 <tatele13@gmail.noreply.github.com>
2024-09-05 23:26:02 -0500

    Almost Finished, Still tweaking PlayGame function

---
+++ README.txt
@@ -0,0 +1,18 @@
+Program: Basic Rock Paper Sissors Program that will involve Html, JavaScript, 
+and CSS. 
+
+The first iteration of this game that will be created will only focus on more 
+of the back-end of the site. The first iteration will entirely be used in the 
+console. 
+
+I will now begin to write out my plan and pseudocode for the functionality. 
+
+RULES 
+1. Rock beats scissors
+2. Scissors beat paper
+3. Paper beats rock.
+
+PLAN 
+1. user will pick a number between 1-3. 
+2. computer will randomly pick a number between 1-3
+3. test between the two and see who won. 
---
+++ javascript.js
@@ -0,0 +1,134 @@
+// global variables
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  // randomly return string "rock" "paper" "sissor"
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    return "rock";
+  } else if (randomNumber === 2) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+function getHumanChoice() {
+  let userInput = prompt("Rock, Paper, Or Scissors? ").toLowerCase();
+
+  // validation
+  while (
+    userInput === null ||
+    (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors")
+  ) {
+    userInput = prompt(
+      "Invalid choice. Please enter Rock, Paper, or Scissors:"
+    ).toLowerCase();
+  }
+  return userInput;
+}
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == computerChoice) {
+    humanScore++;
+    computerScore++;
+    return (
+      "ITS a TIE" +
+      "\n" +
+      "Human Score:" +
+      humanScore +
+      "\n" +
+      "Computer Score:" +
+      computerScore
+    );
+  }
+  if (humanChoice == "rock") {
+    if (computerChoice == "scissors") {
+      humanScore++;
+      return (
+        "Human Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    } else {
+      computerScore++;
+      return (
+        "Computer Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    }
+  }
+  if (humanChoice == "scissors") {
+    if (computerChoice == "paper") {
+      humanScore++;
+      return (
+        "Human Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    } else {
+      computerScore++;
+      return (
+        "Computer Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    }
+  }
+  if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      humanScore++;
+      return (
+        "Human Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    } else {
+      computerScore++;
+      return (
+        "Computer Win" +
+        "\n" +
+        "Human Score:" +
+        humanScore +
+        "\n" +
+        "Computer Score:" +
+        computerScore
+      );
+    }
+  }
+}
+
+// console.log(humanSelection, computerSelection);
+// console.log(playRound(humanSelection, computerSelection));
+function playGame(numOfGames) {
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+  for (let i = 1; i <= numOfGames; i++) {
+    console.log(playRound(humanSelection, computerSelection));
+  }
+}
+
+let numOfGames = parseInt(prompt("How many games would you like to play? "));
+playGame(numOfGames);
